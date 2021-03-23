3298b139515011941598914f5c0f81ba56a6dbfa
Hyndavi Anche <hanche@teisd.com>
2021-03-23 18:31:53 -0500

    Added helper class to add padding to all properties

---
+++ LibrsModels/Classes/Helpers.cs
@@ -0,0 +1,16 @@
+﻿namespace LibrsModels.Classes
+{
+    public static class Helpers
+    {
+        public static string PadL(this string property, int length, char paddingCharacter = ' ')
+        {
+            property = property ?? "";
+            return property.PadLeft(length, paddingCharacter);
+        }
+        public static string PadR(this string property, int length, char paddingCharacter = ' ')
+        {
+            property = property ?? "";
+            return property.PadRight(length, paddingCharacter);
+        }
+    }
+}
