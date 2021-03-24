9761ad1e4e510142e9022710b596d037d2ab885c
Hyndavi Anche <hanche@teisd.com>
2021-03-24 10:24:21 -0500

    Implemented ipaddingFixer in OffUsing

--- LibrsModels/Classes/OffUsing.cs
+++ LibrsModels/Classes/OffUsing.cs
@@ -3,7 +3,7 @@ using Newtonsoft.Json;
 
 namespace LibrsModels.Classes
 {
-    public class OffUsing : LegacyLibrsValues
+    public class OffUsing : LegacyLibrsValues, IPaddingFixer
     {
 
         [JsonProperty("offenderSeqNum")] public string OffenderSeqNum { get; set; } = "   ";
@@ -15,6 +15,12 @@ namespace LibrsModels.Classes
             SegmentDescriptor = "41";
             ExpansionBuffer = string.Concat(Enumerable.Repeat(" ", 20));
             Padding = string.Concat(Enumerable.Repeat(" ", 100));
+        }
+
+        public void FixPaddings()
+        {
+            OffenderSeqNum = OffenderSeqNum.PadL(3);
+            OffUsingGamingMot = OffUsingGamingMot.PadL(4);
         }
     }
 }
