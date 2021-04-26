36e61f2203176b6a8e66a2dcc4437616c49d06db
Ivan Filpo <ifilpo@teisd.com>
2021-04-26 10:32:03 -0500

    Ported over IRootValidationObject and had to create generic for serialization

---
+++ LibrsModels/Classes/IRootValidationObject.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace LibrsModels.Classes
+{
+    public interface IRootValidationObject<T> where T: ILibrsIncident
+    {
+        string Spec { get; }
+        string Ori { get; }
+        int ReportYear { get; }
+        int ReportMonth { get; }
+        bool ForSubmission { get; }
+        List<T> IncidentList { get; }
+        ZeroReport ZeroReport { get; }
+    }
+}
